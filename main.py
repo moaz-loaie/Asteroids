d5dad199a95775633f6931f2084ccdac466f2dd4
moaz-loaie <mo3azloaie@gmail.com>
2026-05-05 23:38:01 +0300

    add collide logic to the game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
+
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -34,6 +36,12 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
